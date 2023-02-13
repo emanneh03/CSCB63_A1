7dd36f4944f569011195031051708dd959eb1dc6
emanneh03 <elenamanneh@hotmail.com>
2023-02-13 14:40:12 -0500

    Fixed delete

--- AVL_tree.c
+++ AVL_tree.c
@@ -37,12 +37,12 @@ void update_height(AVL_Node* node) {
     /* Updates the height of the tree rooted at node 'node' based on the heights
      * of its children. Note: this should be an O(1) operation.
      */
-
+    
     int left_height = 0, right_height = 0;
-
+    
     if (node->left != NULL) left_height = node->left->height;
     if (node->right != NULL) right_height = node->right->height;
-
+    
     node->height = right_height > left_height ? ++right_height: ++left_height;
 }
 
@@ -99,32 +99,32 @@ AVL_Node* left_rotation(AVL_Node* node) {
 
 AVL_Node* right_left_rotation(AVL_Node* node);
 //AVL_Node* right_left_rotation(AVL_Node* node) {
-    
-    /* Returns the result of performing a right/clockwise then left/counter-clockwise rotation in the AVL
-     * tree rooted at 'node'.
-     */
-    
+
+/* Returns the result of performing a right/clockwise then left/counter-clockwise rotation in the AVL
+ * tree rooted at 'node'.
+ */
+
 //    if (node == NULL) return NULL;
 //
 //    node->right = right_rotation(node->right);
 //    node = left_rotation(node);
 //    return node;
-    
+
 //}
 
 AVL_Node* left_right_rotation(AVL_Node* node);
 //AVL_Node* left_right_rotation(AVL_Node* node) {
-    
-    /* Returns the result of performing a left/counter-clockwise then right/clockwise rotation in the AVL
-     * tree rooted at 'node'.
-     */
-    
+
+/* Returns the result of performing a left/counter-clockwise then right/clockwise rotation in the AVL
+ * tree rooted at 'node'.
+ */
+
 //    if (node == NULL) return NULL;
 //
 //    node->left = left_rotation(node->left);
 //    node = right_rotation(node);
 //    return node;
-    
+
 //}
 
 
@@ -163,71 +163,71 @@ AVL_Node* rebalance(AVL_Node* node) {
     /* Rebalances the tree rooted at node 'node'.
      */
     
-//    int balance = balance_factor(node);
-//
-//        // If this node becomes unbalanced, then
-//        // there are 4 cases
-//
-//        // Left Left Case
-//        if (balance > 1 && node->key < node->left->key)
-//            return right_rotation(node);
-//
-//        // Right Right Case
-//        if (balance < -1 && node->key > node->right->key)
-//            return left_rotation(node);
-//
-//        // Left Right Case
-//        if (balance > 1 && node->key > node->left->key)
-//        {
-//            node->left =  left_rotation(node->left);
-//            return right_rotation(node);
-//        }
-//
-//        // Right Left Case
-//        if (balance < -1 && node->key < node->right->key)
-//        {
-//            node->right = right_rotation(node->right);
-//            return left_rotation(node);
-//        }
-//
-//        /* return the (unchanged) node pointer */
-//        return node;
-//
-//
-//    int balance = balance_factor(node);
-//
-//    if (balance > 1) {
-//
-//        AVL_Node* temp = node->left;
-//
-//        if (balance >= 0) {
-//
-//            return right_rotation(node);
-//
-//        } else {
-//
-//            node->left = left_rotation(node->left);
-//            return right_rotation(node);
-//
-//        }
-//
-//    } else if (balance < -1) {
-//
-//        AVL_Node* temp = node->right;
-//
-//        if (balance <= 0) {
-//
-//            return left_rotation(node);
-//
-//        } else {
-//
-//            node->right = right_rotation(node->right);
-//            return left_rotation(node);
-//
-//        }
-//    }
-//
-//    return node;
+    //    int balance = balance_factor(node);
+    //
+    //        // If this node becomes unbalanced, then
+    //        // there are 4 cases
+    //
+    //        // Left Left Case
+    //        if (balance > 1 && node->key < node->left->key)
+    //            return right_rotation(node);
+    //
+    //        // Right Right Case
+    //        if (balance < -1 && node->key > node->right->key)
+    //            return left_rotation(node);
+    //
+    //        // Left Right Case
+    //        if (balance > 1 && node->key > node->left->key)
+    //        {
+    //            node->left =  left_rotation(node->left);
+    //            return right_rotation(node);
+    //        }
+    //
+    //        // Right Left Case
+    //        if (balance < -1 && node->key < node->right->key)
+    //        {
+    //            node->right = right_rotation(node->right);
+    //            return left_rotation(node);
+    //        }
+    //
+    //        /* return the (unchanged) node pointer */
+    //        return node;
+    //
+    //
+    //    int balance = balance_factor(node);
+    //
+    //    if (balance > 1) {
+    //
+    //        AVL_Node* temp = node->left;
+    //
+    //        if (balance >= 0) {
+    //
+    //            return right_rotation(node);
+    //
+    //        } else {
+    //
+    //            node->left = left_rotation(node->left);
+    //            return right_rotation(node);
+    //
+    //        }
+    //
+    //    } else if (balance < -1) {
+    //
+    //        AVL_Node* temp = node->right;
+    //
+    //        if (balance <= 0) {
+    //
+    //            return left_rotation(node);
+    //
+    //        } else {
+    //
+    //            node->right = right_rotation(node->right);
+    //            return left_rotation(node);
+    //
+    //        }
+    //    }
+    //
+    //    return node;
     
     if (height(node->left) - height(node->right) > 1) {
         AVL_Node* temp = node->left;
@@ -343,7 +343,7 @@ AVL_Node* delete(AVL_Node* node, int key) {
         if (node->left == NULL && node->right == NULL) {
             
             free(node);
-            return temp;
+            return NULL;
             
         } else if (node->left == NULL) {
             
@@ -363,15 +363,57 @@ AVL_Node* delete(AVL_Node* node, int key) {
             
             node->key = temp->key;
             node->value = temp->value;
-            update_height(node);
-            
-            node->right = delete(node->right, key);
+            node->right = delete(node->right, temp->key);
             
         }
         
     }
+    update_height(node);
+    return rebalance(node);
     
-    node = rebalance(node);
-    return node;
+    
+    
+    
+//    if (node == NULL){
+//        return NULL;
+//    }
+//    
+//    AVL_Node* temp = node;
+//    
+//    if (node->key > key) {
+//        node->left = delete(node->left, key);
+//    }
+//    else if (node->key < key) {
+//        node->right = delete(node->right, key);
+//    }
+//    else {
+//        if (node->left == NULL && node->right == NULL) {
+//            free(node);
+//            return NULL;
+//        }
+//        else if (node->left == NULL) {
+//            temp = node->right;
+//            free(node);
+//            return temp;
+//        }
+//        
+//        else if (node->right == NULL) {
+//            temp = node->left;
+//            free(node);
+//            return temp;
+//        }
+//        
+//        else {
+//            AVL_Node* succ = successor(node);
+//            node->key = succ->key;
+//            node->value = succ->value;
+//            node->right = delete(node -> right, succ -> key);
+//        }
+//        
+//    }
+//    update_height(node);
+//    node = rebalance(node);
+//    return node;
     
 }
+
